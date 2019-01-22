481bf7e331a322bdab65bb99774547bf5320d1bd
me <temsky87@gmail.com>
2019-01-22 22:05:16 +0500

    HashSet helped me

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,11 +3,9 @@
  */
 public class Program {
     public static void main(String[] args) {
-        long m = System.currentTimeMillis();
         TestTask task = new TestTask("0 5 10 0 11 14 13 4 11 8 8 7 1 4 12 11");
         task.doIt();
         System.out.println("Количество шагов = " + task.getCountStep());
         System.out.println("Длина цикла  = " + task.getLengthCycle());
-        System.out.println((double) (System.currentTimeMillis() - m));
     }
 }
